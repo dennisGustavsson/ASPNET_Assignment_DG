76a48b5d5be89e2cbd5193fec7f024e25fe7574b
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2023-04-17 12:46:49 +0200

    updated database, added controllers

---
+++ EcomWebApp/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcomWebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EcomWebApp/Controllers/ProductsManagerController.cs
@@ -0,0 +1,40 @@
+﻿using EcomWebApp.Services;
+using EcomWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcomWebApp.Controllers;
+
+public class ProductsManagerController : Controller
+{
+    private readonly ProductService _productService;
+
+    public ProductsManagerController(ProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(ProductRegistrationViewModel productRegistrationViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            if (await _productService.CreateAsync(productRegistrationViewModel))
+                return RedirectToAction("Index", "ProductsManager");
+
+            ModelState.AddModelError("", "Något gick fel vid skapandet av produkten");
+        }
+
+        return View(productRegistrationViewModel);
+    }
+
+}
