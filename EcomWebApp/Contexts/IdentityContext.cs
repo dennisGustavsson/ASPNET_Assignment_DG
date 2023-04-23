2445a0a2f24337faceb6e4ab62f2adbea7306c72
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2023-04-23 21:36:55 +0200

    adding role

--- EcomWebApp/Contexts/IdentityContext.cs
+++ EcomWebApp/Contexts/IdentityContext.cs
@@ -1,18 +1,43 @@
 ﻿using EcomWebApp.Models.Entities;
 using EcomWebApp.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace EcomWebApp.Contexts
+namespace EcomWebApp.Contexts;
+
+public class IdentityContext : IdentityDbContext<AppUser>
 {
-    public class IdentityContext : IdentityDbContext<AppUser>
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
     {
-        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<AddressEntity> AspNetAddresses { get; set; }
+    public DbSet<AddressEntity> AspNetAddresses { get; set; }
 
-        public DbSet<UserAddressEntity> AspNetUsersAddresses { get; set; }
+    public DbSet<UserAddressEntity> AspNetUsersAddresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Name = "System Administrator",
+                NormalizedName = "SYSTEM ADMINISTRATOR"
+            }
+            );
+
+
+        var passwordHasher = new PasswordHasher<AppUser>();
+        builder.Entity<AppUser>().HasData(
+            new AppUser
+            {
+                FirstName = "System",
+                LastName = "Administrator",
+                UserName = "administrator",
+                PasswordHash = passwordHasher.HashPassword(null!,"BytMig123!"),
+                
+            });
     }
 }
---
+++ EcomWebApp/Migrations/Identity/20230423185523_Seed Roles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcomWebApp.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f3ae3278-6f9a-41bb-9d21-a7a963d67980", null, "System Administrator", "SYSTEM ADMINISTRATOR" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f3ae3278-6f9a-41bb-9d21-a7a963d67980");
+        }
+    }
+}
