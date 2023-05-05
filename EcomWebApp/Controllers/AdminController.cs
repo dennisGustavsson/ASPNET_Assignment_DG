b92a4bd5c24f42bde4c6edd33a657cb2b61734cc
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2023-05-05 17:04:22 +0200

    working on styling usermanager

--- EcomWebApp/Controllers/AdminController.cs
+++ EcomWebApp/Controllers/AdminController.cs
@@ -3,7 +3,6 @@ using EcomWebApp.Models.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace EcomWebApp.Controllers;
 
